fbe791828dfb8c7256f1481c44d9d4a3524f1ac8
zephyr5028 <marguerie.jl@wanadoo.fr>
2023-11-20 17:52:49 +0100

    ajout du module ir au convoyeur

--- module_convoyeur/convoyeur_arduino/convoyeur_arduino.ino
+++ module_convoyeur/convoyeur_arduino/convoyeur_arduino.ino
@@ -2,38 +2,23 @@
     projet robocup 2024
     convoyeur
     jlm nov 2023
-    nema 17 sur X (shield CNC arduino uno)
     https://passionelectronique.fr/tutoriel-l298n/
 ************************************************/
 
 //=============================
 // inclusion des bibliothèques
 //=============================
-//#include "Timer.h"      //http://github.com/JChristensen/Timer
-#include <Wire.h>       //composants utilisant le protocole I2C
+#include <Wire.h>  //composants utilisant le protocole I2C
 #include "variables.h"
-
-//Timer timer;                        //instantiate the timer object
 
 //============
 // sensor IR
 //============
 bool sensorIR() {
-  //int sensorStatus = digitalRead(IRSensor); // Set the GPIO as Input
-  int sensorStatus = 1;
-
-  //Serial.print("sensor statut : ");
-  //Serial.print(sensorStatus);
-  // Check if the pin high or not
-  if (sensorStatus == 1)   {
-    // if the pin is high turn off the onboard Led
-    digitalWrite(LED, LOW); // LED LOW
-    //Serial.println("   objet absent");      // print Motion Detected! on the serial monitor window
-  }
-  else  {
-    //else turn on the onboard LED
-    digitalWrite(LED, HIGH);                 // LED High
-    //Serial.println("   objet present");         // print Motion Ended! on the serial monitor window
+  bool sensorStatus = digitalRead(IRSensor);  // read IRsensor
+  if (debug) {
+    Serial.print("sensor statut : ");
+    Serial.println(sensorStatus);
   }
   return sensorStatus;
 }
@@ -46,27 +31,14 @@ void setup() {
   Serial.println();
   Serial.println("Demarrage du convoyeur avec le sensor ir");
 
-  // Configuration des pins de l'Arduino en "sortie" 
+  // Configuration des pins de l'Arduino en "sortie"
   pinMode(borneENB, OUTPUT);
   pinMode(borneIN3, OUTPUT);
   pinMode(borneIN4, OUTPUT);
-  digitalWrite(borneENB, LOW);        // arret du moteur 1
-  
-/*
-  // stepper nema17
-  pinMode(enPin, OUTPUT);  // enable driver X
-  digitalWrite(enPin, LOW);
-  pinMode(stepPin, OUTPUT);  // impulsion effectue une step.
-  pinMode(dirPin, OUTPUT);   // permet d'indiquer la direction de rotation du moteur
-  */
-/*
-  // timer entre deux implulsions - utilise milli()
-  timer.oscillate(stepPin, motorSpeed, HIGH);
-  timer.oscillate(stepPin, motorSpeed, LOW);
-*/
+
   // sensor ir
-  pinMode(IRSensor, INPUT); // IR Sensor pin INPUT
-  pinMode(LED, OUTPUT);     // LED Pin Output
+  pinMode(IRSensor, INPUT);  // IR Sensor pin INPUT
+  pinMode(LED, OUTPUT);      // LED Pin Output
 
 #if I2C
   // Initialise la library Wire et se connecte au bus I2C en tant qu'esclave
@@ -81,7 +53,6 @@ void setup() {
 // wire
 //======
 #if I2C
-// Wire
 // requestEvents
 void requestEvents() {
   if (requestI2C) {
@@ -90,7 +61,6 @@ void requestEvents() {
     requestI2C = 0;
   }
 }
-// Wire
 // receiveEventys
 void receiveEvents(int howMany) {
   // Check for incoming data
@@ -100,160 +70,190 @@ void receiveEvents(int howMany) {
     for (int i = 0; i < MAX_MESSAGE_LENGTH; i++) {
       message[i] = '\0';
     }
-    // prints the received data
-    //Serial.print("I received: ");
-    for (int i = 0; i < MAX_MESSAGE_LENGTH ; i++) {
-      dataI2C =  Wire.read(); // lecture de l'i2c
-      if (dataI2C == 0xFFFFFFFF ) {  // fin du message avant la longueur maxi
+    if (debug) {
+      // prints the received data
+      Serial.print("I received: ");
+    }
+    for (int i = 0; i < MAX_MESSAGE_LENGTH; i++) {
+      dataI2C = Wire.read();        // lecture de l'i2c
+      if (dataI2C == 0xFFFFFFFF) {  // fin du message avant la longueur maxi
         break;
       }
       message[i] = dataI2C;  // constitution du message
-      //Serial.print(dataI2C,HEX);
-      //Serial.print(message[i]);
+      if (debug) {
+        Serial.print(dataI2C, HEX);
+        Serial.print(message[i]);
+      }
     }
-    //Serial.println("");
     //int taille = sizeof(message); // taille du tableau
-    int taille = strlen(message); // taille de la chaine
-    //Serial.print("taille message : ");
-    //Serial.println(taille);
-
+    int taille = strlen(message);  // taille de la chaine
+    if (debug) {
+      Serial.println("");
+      Serial.print("taille message : ");
+      Serial.println(taille);
+    }
     char dataInS[taille];
-
-    // reception du message sc
-    if (message[0] == 115 and message[1] == 99) { // s situation et c convoyeur
-      requestI2C = 1; // demande du master i2c
-      //Serial.println(requestI2C);
-    }
-
-    // reception du message mvxx vitesse rpm
-    if (message[0] == 109 and message[1] == 118) { // m motor et v vitesse speed
-      // Extract only the number. E.g. from "mv10" to "10"
-      for (int i = 0; i < taille; i++) {
-        dataInS[i] = message[i + 2];
+    // reception du message c convoyeur
+    if (message[0] == 99) {  // c convoyeur
+      requestI2C = 1;        // demande du master i2c
+      if (debug) {
+        Serial.println(requestI2C);
       }
-      motorSpeed = atoi(dataInS); // char to int
-      //Serial.print("Motor speed : ");
-      //Serial.println(motorSpeed);
     }
-  /*
-    // reception du message mpxx pas
-    if (message[0] == 109 and message[1] == 112) { // m motor et p pas
-      // Extract only the number. E.g. from "mp120" to "120"
+    // reception du message vxxx vitesse 60 to 255
+    if (message[1] == 118) {  // v vitesse speed
+      // Extract only the number. E.g. from "v150" to "150"
       for (int i = 0; i < taille; i++) {
-        dataInS[i] = message[i + 2];
+        dataInS[i] = message[i + 1];
       }
-      motorStep = atoi(dataInS); // char to int
-      //Serial.print("motor step : ");
-      //Serial.println(motorStep);
+      motorSpeed = atoi(dataInS);  // char to int
+      if (debug) {
+        Serial.print("Motor speed : ");
+        Serial.println(motorSpeed);
+      }
     }
-    */
-    // reception du message msx sens
-    if (message[0] == 109 and message[1] == 115) { // m motor et s sens
-      // Extract only the number. E.g. from "ms1" to "1"
+    // reception du message sx sens
+    if (mmessage[1] == 115) {  // s sens
+      // Extract only the number. E.g. from "s1" to "1"
       for (int i = 0; i < taille; i++) {
-        dataInS[i] = message[i + 2];
+        dataInS[i] = message[i + 1];
       }
-      motorInverse = atoi(dataInS); // char to int
-      //Serial.print("Motor Inverse : ");
-      //Serial.println(motorInverse);
+      motorSens = atoi(dataInS);  // char to int, 1 marche avant, 0 marche arriere
+      if (debug) {
+        Serial.print("Motor Inverse : ");
+        Serial.println(motorSens);
+      }
     }
-    // reception du message STOP
-    if (message[0] == 83 and message[1] == 84 and message[2] == 79 and message[3] == 80) {  // STOP
-      stopStart = 0;
-      //Serial.print("Stop : ");
-      //Serial.println(stopStart);
+    // reception du message arret
+    if (message[0] == 65) {  // A pour arret
+      arret = 1;
+      marche = 0;
+      if (debug) {
+        Serial.print("Arret : ");
+        Serial.println(arret);
+      }
     }
-    // reception du message START
-    if (message[0] == 83 and message[1] == 84 and message[2] == 65 and message[3] == 82 and message[4] == 84) {  // START
-      stopStart = 1;
-      //Serial.print("Start : ");
-      //Serial.println(stopStart);
+    // reception du message marche
+    if (message[0] == 77) {  // M pour marche
+      marche = 1;
+      arret = 0;
+      if (debug) {
+        Serial.print("Marche : ");
+        Serial.println(smarche);
+      }
     }
   }
 }
 #endif
 
-//=============================
-// Fonction : lancerRotationMoteurPontA()
-// But :      Active l'alimentation du moteur connecte sur le pont A
-//            pendant 2 secondes, puis le met à l'arret (au moins 1 seconde)
-//=============================
-void lancerRotationMoteurPontB() {
-  digitalWrite(borneENB, HIGH);       // Active l'alimentation du moteur 1
-  delay(5000);                        // et attend 2 secondes
-  
-  digitalWrite(borneENB, LOW);        // Desactive l'alimentation du moteur 1
-  delay(1000);                        // et attend 1 seconde
+//=========================================
+// Fonction configurerSensDeRotationPontB()
+// Definit le sens de rotation du moteur
+//=========================================
+void configurerSensDeRotationPontB(bool sensDeRotation) {
+
+  if (sensDeRotation == MARCHE_AVANT) {
+    // L298N en "marche avant", Selon sa table de verite
+    digitalWrite(borneIN3, HIGH);  // IN3 niveau haut
+    digitalWrite(borneIN4, LOW);   // IN4 niveau bas
+  }
+
+  if (sensDeRotation == MARCHE_ARRIERE) {
+    // L298N en "marche avant", Selon sa table de verite
+    digitalWrite(borneIN3, LOW);   // IN3 niveau bas
+    digitalWrite(borneIN4, HIGH);  // IN4 niveau haut
+  }
+}
+//=========================================================================
+// Fonction changeVitesseMoteurPontB()
+// Change la vitesse de rotation du moteur (valeur comprise entre 0 et 255)
+//=========================================================================
+void changeVitesseMoteurPontB(int nouvelleVitesse) {
+  // signal PWM rapport cyclique "nouvelleVitesse"
+  analogWrite(borneENB, nouvelleVitesse);
 }
 
 //======
 // loop
 //======
 void loop() {
-  // timer entre deux implulsions - utilise milli()
-  //timer.oscillate(stepPin, motorSpeed, HIGH);
-  //timer.oscillate(stepPin, motorSpeed, LOW);
-  /*
-  // sens de rotation
-  if (motorInverse) {
-    //sens horaire, LOW pour inverser
-    digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
-  } else {
-    digitalWrite(dirPin, LOW); //Changes the rotations direction
+  //if (marche and !objetPresent) {  // demarrage progessif une seule fois
+  if (marche ) {  // demarrage progessif une seule fois
+    //avant
+    //sens de rotation du moteur en "marche avant"
+    configurerSensDeRotationPontB(MARCHE_AVANT);
+
+    // depart progresif du moteur, vitesse maxi 255
+    for (vitesse = vitesseMinimale; vitesse < vitesseMaximale; vitesse++) {
+      changeVitesseMoteurPontB(vitesse);  // vitesse moteur avec signal PWM
+      delay(delaiChangementVitesse);
+    }
+    marche = 0;
   }
 
-  // stop start
-  if ( sensorIR() and stopStart) {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(pulseWidthMicros);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(pulseWidthMicros);
-    //timer.update(); // mise a jour du timer - utilise milli
+  // Check if the pin high or not IRsensor
+  if (sensorIR() == 1) {  // objet absent
+    // if the pin is high turn off the onboard Led
+    digitalWrite(LED, LOW);  // LED LOW
+    if (objetPresent == 1) {
+      marche = 1;
+    }
+    objetPresent = 0;  // pas d'objet 0
+    if (debug) {
+      Serial.println("objet absent");
+    }
   }
-  */
+  if (sensorIR() == 0) {  // objet present
+    //else turn on the onboard LED
+    digitalWrite(LED, HIGH);  // LED High
+    // arret moteur    
+    changeVitesseMoteurPontB(0);
+    objetPresent = 1;  // objet present 1
+    if (debug) {
+      Serial.println("objet present");
+    }
+  }
+
 
 #if TEST
-// Configuration du L298N en "marche avant", pour le moteur connecte au pont A. Selon sa table de verite, il faut que :
-  digitalWrite(borneIN3, HIGH);                 // L'entree IN1 doit etre au niveau haut
-  digitalWrite(borneIN4, LOW);                  // L'entree IN2 doit etre au niveau bas
+  //avant
+  //sens de rotation du moteur en "marche avant"
+  configurerSensDeRotationPontB(MARCHE_AVANT);
 
-  // Et on lance le moteur (branche sur le pont A du L298N)
-  lancerRotationMoteurPontB();
-
-  // Puis on configure le L298N en "marche arriere", pour le moteur connecte sur le pont A.
-  digitalWrite(borneIN3, LOW);                  // L'entree IN1 doit être au niveau bas
-  digitalWrite(borneIN4, HIGH);                 // L'entree IN2 doit être au niveau haut
-
-  // Et on relance le moteur (branche sur le pont A du L298N)
-  lancerRotationMoteurPontB();
-
-/*
-// test du stepper nema17
-  Serial.println(F("Running clockwise"));
-  //sens horaire, LOW pour inverser
-  digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
-
-  // Makes 200 pulses for making one full cycle rotation
-  for (int i = 0; i < stepsPerRev; i++) {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(pulseWidthMicros);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(pulseWidthMicros);
+  // depart progresif du moteur, vitesse maxi 255
+  for (vitesse = vitesseMinimale; vitesse < vitesseMaximale; vitesse++) {
+    changeVitesseMoteurPontB(vitesse);  // vitesse moteur avec signal PWM
+    delay(delaiChangementVitesse);
   }
-  delay(1000); // One second delay
-
-  Serial.println(F("Running counter-clockwise"));
-  digitalWrite(dirPin, LOW); //Changes the rotations direction
-
-  // Makes 400 pulses for making two full cycle rotation
-  for (int i = 0; i < 2 * stepsPerRev; i++) {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(pulseWidthMicros);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(pulseWidthMicros);
+  delay(10000);
+  // deceleration progressive
+  for (vitesse = vitesseMaximale; vitesse > vitesseMinimale; vitesse--) {
+    changeVitesseMoteurPontB(vitesse);  // vitesse moteur avec signal PWM
+    delay(delaiChangementVitesse);
   }
+
+  // arret moteur
+  changeVitesseMoteurPontB(0);
   delay(1000);
-  */
-#endif
 
+  //arriere
+  //sens de rotation du moteur en "marche arriere"
+  configurerSensDeRotationPontB(MARCHE_ARRIERE);
+
+  //depart progresif du moteur, vitesse maxi 255
+  for (vitesse = vitesseMinimale; vitesse < vitesseMaximale; vitesse++) {
+    changeVitesseMoteurPontB(vitesse);  // vitesse moteur avec signal PWM
+    delay(delaiChangementVitesse);
+  }
+  delay(10000);
+  // deceleration progressive
+  for (vitesse = vitesseMaximale; vitesse > vitesseMinimale; vitesse--) {
+    changeVitesseMoteurPontB(vitesse);  // vitesse moteur avec signal PWM
+    delay(delaiChangementVitesse);
+  }
+
+  // arret moteur
+  changeVitesseMoteurPontB(0);
+  delay(1000);
+#endif
 }
--- module_convoyeur/convoyeur_arduino/variables.h
+++ module_convoyeur/convoyeur_arduino/variables.h
@@ -1,26 +1,41 @@
-//========================================================
-// Controls supression of the MonPrintf function to serial
-//========================================================
+//=========
+// Controls
+//=========
 #define SERIAL_BAUD 115200   // vitesse moniteur serie
 #define SerialMonitor   1
-#define debug 1  // true for use Display log message if True
 
 //========================================================
 // Differentes fonctions du programme
 //========================================================
-#define TEST 1       // if 1 true,  0 false
-#define I2C  0        // if 1 true,  0 false
+#define TEST 0       // if 1 true,  0 false
+#define I2C  0       // if 1 true,  0 false
+#define debug 1      // for use Display log message if True
 
 //==========================================
 // Affectation des broches de l'arduino uno
-//==========================================
-//====================================
 // Association des entrees du L298N
 //====================================
-#define borneENB        7       // On associe la borne "ENA" du L298N a la pin D10 de l'arduino
-#define borneIN3        9       // On associe la borne "IN1" du L298N a la pin D9 de l'arduino
-#define borneIN4        8       // On associe la borne "IN2" du L298N a la pin D8 de l'arduino
+#define borneENB        9       // On associe la borne "ENB" du L298N a la pin D9 de l'arduino
+#define borneIN3        8       // On associe la borne "IN3" du L298N a la pin D8 de l'arduino
+#define borneIN4        7       // On associe la borne "IN4" du L298N a la pin D7 de l'arduino
 
+//==========
+// Sensor IR
+//==========
+#define IRSensor        6        // connect IR sensor module to Arduino pin D6
+#define LED             4        // connect LED to Arduino pin D4
+
+//*************************//
+// Constantes du programme //
+//*************************//
+#define delaiChangementVitesse     12       // delai en millisecondes
+#define vitesseMinimale            60       // Rapport cylique minimal du signal PWM, pour faire tourner le moteur au minimum de sa vitesse
+#define vitesseMaximale            255      // Rapport cylique maximal du signal PWM, pour faire tourner le moteur au maximum de sa vitesse
+
+const bool MARCHE_AVANT   = 1;            // constante  "marche avant"
+const bool MARCHE_ARRIERE = 0;            // constante  "marche arrière"
+
+int vitesse;         // vitesse de rotation du moteur, de (0) a (255) <= signal PWM
 
 //==================================================
 // Definition de l'adresse logique de l'esclave I2C
@@ -28,34 +43,14 @@
 #define I2C_SLAVE_CONVOYEUR  12
 
 //==========
-// Sensor IR
-//==========
-int IRSensor = 12; // connect IR sensor module to Arduino pin D12
-int LED = 13;      // connect LED to Arduino pin 13
-
-//==========
 // Variables
 //==========
-/*
-//==================
-// stepper
-//==================
-// nema 17 sur X
-const int enPin = 8;    // enable driver X
-const int stepXPin = 2; // X.STEP  envoyer une impulsion effectue une step.
-const int dirXPin = 5;  // X.DIR  permet d'indiquer la direction de rotation du moteur
 
-int stepPin = stepXPin;
-int dirPin = dirXPin;
-
-int stepsPerRev = 200;        // nombre de pas  200 pour un moteur NEMA17 en full step , 2048 pour un 28BYJ-48
-int pulseWidthMicros  = 1500; // microsecondes - 2400 lent, 500 rapide - 2400 to 500, donc 24x100 to 5x100
-*/
-unsigned long motorSpeed = 2 ;   // 1 rapide , 2 moyen et 4 lent 
-//int motorStep = 20;            // 200 donc 20x10
-bool motorInverse = 1;           // sens
-bool stopStart = 1;              // 0 stop et 1 start
-
+int motorSpeed = 6 ;   // 6 to 25
+bool motorSens = 1;          // 1 marche avant, 0 marche arriere
+bool marche = 1;              // 1 start
+bool arret = 0;               // 1 stop
+bool objetPresent = 0;        // pas d'objet 0
 
 //=====
 // I2C
